1d9da1b57ac06990183921901352a6a35281dbd7
opcoder0 <110003254+opcoder0@users.noreply.github.com>
2023-09-09 09:48:05 +1000

    Basic hello world fuse implementation

--- main.go
+++ main.go
@@ -1,10 +1,60 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
+	"log"
 	"os"
+	"syscall"
+
+	"bazil.org/fuse"
+	"bazil.org/fuse/fs"
 )
+
+type ZFuse struct{}
+type ZDir struct{}
+type ZFile struct{}
+
+var (
+	greeting = "Hello from the fuse file"
+)
+
+var dirs = []fuse.Dirent{
+	{Inode: 2, Name: "hello", Type: fuse.DT_File},
+}
+
+func (ZFuse) Root() (fs.Node, error) {
+	return ZDir{}, nil
+}
+
+func (ZDir) Attr(ctx context.Context, a *fuse.Attr) error {
+	a.Inode = 1
+	a.Mode = os.ModeDir | 0o555
+	return nil
+}
+
+func (ZDir) Lookup(ctx context.Context, name string) (fs.Node, error) {
+	if name == "hello" {
+		return ZFile{}, nil
+	}
+	return nil, syscall.ENOENT
+}
+
+func (ZDir) ReadDirAll(ctx context.Context) ([]fuse.Dirent, error) {
+	return dirs, nil
+}
+
+func (ZFile) Attr(ctx context.Context, a *fuse.Attr) error {
+	a.Inode = 2
+	a.Mode = 0o444
+	a.Size = uint64(len(greeting))
+	return nil
+}
+
+func (ZFile) ReadAll(ctx context.Context) ([]byte, error) {
+	return []byte(greeting), nil
+}
 
 func main() {
 
@@ -20,4 +70,12 @@ func main() {
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
+
+	c, err := fuse.Mount(mountPoint, fuse.FSName("zfuse"), fuse.Subtype("zfuse"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer c.Close()
+
+	err = fs.Serve(c, ZFuse{})
 }
